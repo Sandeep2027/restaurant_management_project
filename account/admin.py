3d19b75deaa513c581dab01a96ecc8c91a5cfbc1
Sandeep2027 <166156895+Sandeep2027@users.noreply.github.com>
2025-08-05 11:41:49 +0530

    Update admin.py

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'name', 'email', 'phone_number')
+    search_fields = ('user__username', 'name', 'email', 'phone_number')
+    readonly_fields = ('user',)
