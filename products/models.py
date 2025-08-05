3ca587ba9c9f1f2d85a238faa9c510f75c418b5b
Sandeep2027 <166156895+Sandeep2027@users.noreply.github.com>
2025-08-05 11:54:18 +0530

    Update models.py

--- products/models.py
+++ products/models.py
@@ -7,4 +7,20 @@ class Item(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
+
         return str(self.item_name)
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['name']),
+        ]
+
+    def __str__(self):
+        return self.name
