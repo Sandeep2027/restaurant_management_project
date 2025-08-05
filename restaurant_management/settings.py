5c7d4ca160ed4b4f5d8323a9adfcd2d9951cb2bd
Sandeep2027 <166156895+Sandeep2027@users.noreply.github.com>
2025-08-05 11:44:40 +0530

    Update settings.py

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -37,12 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'rest_framework',
-    'home',
+    'rest_framework.authtoken',
+    'notes',
+    'notes',
     'account',
-    'products',
     'orders',
+    'products',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
