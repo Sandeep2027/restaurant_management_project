46bd6b73131d789d1e093b89d0fd0c84f8997017
Sandeep2027 <166156895+Sandeep2027@users.noreply.github.com>
2025-08-05 11:46:55 +0530

    Update urls.py

--- restaurant_management/urls.py
+++ restaurant_management/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,5 +25,6 @@ urlpatterns = [
     path('api/products/',include('products.urls')),
     path('api/orders/',include('orders.urls')),
 ]
+
 
 
