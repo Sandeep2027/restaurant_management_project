fbc71f233514140ff5cf6643e337d4fa47be860f
Sandeep2027 <166156895+Sandeep2027@users.noreply.github.com>
2025-08-05 11:54:59 +0530

    Update serializers.py

--- products/serializers.py
+++ products/serializers.py
@@ -1,7 +1,16 @@
 from rest_framework import serializers
 from .models import Item
+from .models import Product
 
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
         fields = '__all__'
+
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'description', 'price', 'created_at']
+        read_only_fields = ['created_at']
