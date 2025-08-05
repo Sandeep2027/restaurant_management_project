d6c904d8eb2dc71405f6448e2158e371fd5c8616
Sandeep2027 <166156895+Sandeep2027@users.noreply.github.com>
2025-08-05 22:18:35 +0530

    Update admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
-from .models import *
+from .models import Product
 
-
-# Custom Admins
 class ItemAdmin(admin.ModelAdmin):
     list_display = ['item_name','item_price','created_at']
 
-
-# Register your models here.
 admin.site.register(Item,ItemAdmin)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('name', 'description')
+    date_hierarchy = 'created_at'
+    ordering = ('name',)
+    fields = ('name', 'description', 'price', 'created_at')
+    readonly_fields = ('created_at',)
