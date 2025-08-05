ee172b6635ca97b40ec724ee992f447f4ff2439d
Sandeep2027 <166156895+Sandeep2027@users.noreply.github.com>
2025-08-05 11:59:02 +0530

    Update views.py

--- home/views.py
+++ home/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 import requests
 from django.conf import settings
+
+def home_view(request):
+    api_url = f"{settings.SITE_URL}/api/products/"
+    try:
+        response = requests.get(api_url)
+        response.raise_for_status()
+        products = response.json()
+    except requests.RequestException:
+        products = []
+    return render(request, 'home/index.html', {'products': products})
+
