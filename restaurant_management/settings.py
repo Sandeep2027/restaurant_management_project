c2e5e28b2c30a2408381d6b9197d447453109f96
Sandeep2027 <166156895+Sandeep2027@users.noreply.github.com>
2025-08-13 22:27:42 +0530

    Update settings.py

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -141,8 +141,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DEBUG = True  
 RESTAURANT_NAME="MASTER CHEFF"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
 
 
 
