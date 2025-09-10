2e87a085c203573a348febb69f125e40b59a5235
Sandeep2027 <166156895+Sandeep2027@users.noreply.github.com>
2025-09-10 22:52:56 +0530

    Update models.py

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Feedback(models.Model):
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"Feedback {self.id}"
