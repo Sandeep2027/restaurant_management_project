17ae6048cabe58eddcaea9dbc36d809a90a48ea9
Sandeep2027 <166156895+Sandeep2027@users.noreply.github.com>
2025-09-10 07:35:22 +0530

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from  .views import home, about, contact
+from  .views import home, about, contact,  reservations
 
 urlpatterns = [
     path('', home, name='home'),
     path('about/', about, name='about'),
     path('contact/', contact, name='contact'),
+  path('reservations/', reservations, name='reservations'),
 ]
-
-
