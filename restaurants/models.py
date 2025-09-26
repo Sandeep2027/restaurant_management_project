4ef6ee82b23375724ef373b453b6d0b71cc1b250
Sandeep2027 <166156895+Sandeep2027@users.noreply.github.com>
2025-09-26 22:08:44 +0530

    Create models.py

---
+++ restaurants/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100)
+    owner_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=15)
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['name']),
+        ]
+
+    def __str__(self):
+        return self.name
