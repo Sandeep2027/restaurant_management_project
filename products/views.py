88b38835e540073ab1b59071c6a297399e6074b3
Sandeep2027 <166156895+Sandeep2027@users.noreply.github.com>
2025-09-26 22:00:23 +0530

    Update views.py

--- products/views.py
+++ products/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from rest_framework import status, viewsets
-from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from rest_framework.decorators import action
@@ -9,29 +8,21 @@ from .models import Product, Item, MenuItem
 from .serializers import ProductSerializer, ItemSerializer, MenuItemSerializer
 
 
-# Item APIView
-class ItemView(APIView):
-    def get(self, request):
-        items = Item.objects.all()
-        serializer = ItemSerializer(items, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def post(self, request):
-        serializer = ItemSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# Item CRUD using ModelViewSet (replaces ItemView class)
+class ItemViewSet(viewsets.ModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+    permission_classes = [AllowAny]
 
 
-# Product ViewSet (readonly)
+# Product ViewSet (read-only)
 class ProductViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [AllowAny]
 
 
-# Menu View with CRUD using ViewSet
+# MenuItem CRUD + custom menu view
 class MenuItemViewSet(viewsets.ModelViewSet):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
@@ -42,13 +33,7 @@ class MenuItemViewSet(viewsets.ModelViewSet):
             return MenuItem.objects.filter(restaurant_id=restaurant_id)
         return super().get_queryset()
 
-
     @action(detail=False, methods=['get'])
     def menu_view(self, request):
-        # Hardcoded menu items for now (can be replaced with DB query later)
-        menu_items = [
-            {"name": "Paneer Butter Masala", "description": "Creamy paneer dish with rich tomato sauce", "price": 12.99, "is_available": True},
-            {"name": "Margherita Pizza", "description": "Classic pizza with mozzarella and basil", "price": 10.99, "is_available": True},
-            {"name": "Chocolate Lava Cake", "description": "Warm cake with a molten chocolate center", "price": 5.99, "is_available": False},
-        ]
+        menu_items = MenuItem.objects.all()
         return render(request, 'products/menu.html', {'menu_items': menu_items})
